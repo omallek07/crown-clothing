f95f3a08f5c702c6fe779a6feb487937c11e3112
Kevin <omallek@gmail.com>
2019-08-07 06:47:41 -0500

    created homepage and menu items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import HomePage from './pages/homepage/homepage.component';
 import './App.css';
-import HomePage from './homepage.component';
 
 function App() {
   return (
