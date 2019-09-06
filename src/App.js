096e772dae7e751597243de844e3dab97aea911a
Kevin <omallek@gmail.com>
2019-09-06 14:27:37 -0500

    added checkout page, checkout item component and updated cart to use selecters

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import "./App.css";
 import { connect } from "react-redux";
 import { Route, Switch, Redirect } from "react-router-dom";
+import { createStructuredSelector } from 'reselect';
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import CheckoutPage from './pages/checkout/checkout.component';
 
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { selectCurrentUser } from './redux/user/user.selectors';
 import { setCurrentUser } from "./redux/user/user.actions";
 
 class App extends React.Component {
@@ -43,6 +46,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             exact
             path="/signin"
@@ -60,8 +64,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 });
 
 // Dispatch lets redux know an action is being passed
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -7,10 +7,37 @@ export const selectCartItems = createSelector(
   cart => cart.cartItems
 );
 
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
+);
+
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
-  cartItems => cartItems.reduce(
-    (accumQuantity, cartItem) => accumQuantity + cartItem.quantity,
-    0
-  )
+  cartItems =>
+    cartItems.reduce(
+      (accumQuantity, cartItem) => accumQuantity + cartItem.quantity,
+      0
+    )
 );
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumQuantity, cartItem) =>
+        accumQuantity + cartItem.quantity * cartItem.price,
+      0
+    )
+);
+
+/*
+Reselect:
+
+Selectors can compute derived data, allowing Redux to store the minimal possible state.
+
+Selectors are efficient. A selector is not recomputed unless one of its arguments changes.
+
+Selectors are composable. They can be used as input to other selectors.
+
+*/
