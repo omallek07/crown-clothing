6750fb4cc13d22272d013c0475729ae0ac16b461
Kevin <omallek@gmail.com>
2019-09-06 16:41:24 -0500

    adds persistence to cart items

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 
-import rootReducer from './root-reducer';
+import logger from "redux-logger";
+
+import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
+const persistor = persistStore(store);
 
-export default store;
+export { store, persistor };
