fa3386e1e7f94ecf29d6238abed92df3fa8a69cd
Kevin <omallek@gmail.com>
2019-11-19 07:46:43 -0600

    fixes another typo

--- client/src/redux/root-saga.js
+++ client/src/redux/root-saga.js
@@ -1,8 +1,8 @@
 import { all, call } from 'redux-saga/effects';
 
-import { userSagas } from './user/user.types';
-import { cartSagas } from './cart/cart.types';
-import { shopSagas } from './shop/shop.types';
+import { userSagas } from './user/user.sagas';
+import { cartSagas } from './cart/cart.sagas';
+import { shopSagas } from './shop/shop.sagas';
 
 export default function* rootSaga() {
   yield all([
