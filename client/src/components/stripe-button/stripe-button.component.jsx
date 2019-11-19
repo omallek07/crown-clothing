cadd6764ab3d96b3c4f8868313d47beb1f9ed772
Kevin <omallek@gmail.com>
2019-11-19 07:13:58 -0600

    adds axios on client side for stripe

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_svtKx5jb3EPxWtDyafxmw7dr";
   const onToken = token => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    }).then(response => {
+      alert('Payment was successful');
+    }).catch(error => {
+      console.log('Payment error: ', JSON.parse(error));
+      alert('There was an issue with your payment. Please make sure you use the provided credit card');
+    });
   };
 
   return (
